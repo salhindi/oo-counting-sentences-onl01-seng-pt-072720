24cff9ae6557bd0f693087781f2d143ec5725d7a
Sara A <calisaraalhindi@gmail.com>
2020-08-17 22:42:07 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
-  end
+    self.end_with?("!")
+    end
 
   def count_sentences
-
+    self.split(/[.!?]/).reject {|a| a.empty?}.size
   end
 end
