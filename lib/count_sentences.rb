4cf9c64dbabab36444de17fa627b202724b99479
Sara A <calisaraalhindi@gmail.com>
2020-08-17 22:40:47 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,6 @@ class String
     end
 
   def count_sentences
-    array = [".", "?", "!"]
-self.split[/.!?/].reject {|a| a.empty?}.size
+    self.split[/.!?/].reject {|a| a.empty?}.size
   end
 end
